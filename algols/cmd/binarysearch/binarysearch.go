dad09d34b6545610529c18aed342791724385a20
Preston Neal <mrprestonneal@gmail.com>
2023-09-30 12:18:45 -0700

    binary search works for ints

--- algols/cmd/binarysearch/binarysearch.go
+++ algols/cmd/binarysearch/binarysearch.go
@@ -1,5 +1,28 @@
 package binarysearch
 
-func binarysearch() int {
+func BinarySearch(list []int, search int) int {
+    low := 0
+    high := len(list) - 1
+
+    if high == -1 {
+        return -1
+    }
+    
+    if high == low && list[0] == search {
+        return 0
+    }
+
+    for low <= high {
+        mid := (low + high) / 2
+        guess := list[mid]
+        if guess == search {
+            return mid
+        } else if guess > search {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+
     return -1
 }
--- algols/cmd/binarysearch/binarysearch_test.go
+++ algols/cmd/binarysearch/binarysearch_test.go
@@ -2,11 +2,24 @@ package binarysearch
 
 import "testing"
 
-func TestBinarySearchRetunsNegativeOneWhenNotFound(t *testing.T) {
-    got := binarysearch()
-    want := -1
+type testCase struct {
+    list []int
+    search_for int
+    expected int
+}
 
-    if want != got {
-        t.Fatalf("wanted %v, got %v", want, got)
+var testCases = []testCase {
+    {[]int{}, 1, -1},
+    {[]int{1}, 1, 0},
+    {[]int{1,2,3}, 2, 1},
+    {[]int{1,2,3,4,5,6}, 6, 5},
+    {[]int{1,2,3,4,5,6}, 9, -1},
+}
+
+func TestBinarySearch(t *testing.T) {
+    for _, test := range testCases {
+        if output := BinarySearch(test.list, test.search_for); output != test.expected {
+            t.Errorf("Output %v not equal to expected %v", output, test.expected)
+        }
     }
 }
