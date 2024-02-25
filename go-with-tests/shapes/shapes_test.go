aa921004818fc0c945e4918bd050f9a73f2a6c70
Preston Neal <mrprestonneal@gmail.com>
2024-02-25 09:14:47 -0800

    refactor test

--- go-with-tests/shapes/shapes_test.go
+++ go-with-tests/shapes/shapes_test.go
@@ -12,25 +12,30 @@ func TestPerimeter(t *testing.T) {
     }
 }
 
-func TestArea(t *testing.T) {
-    t.Run("rectangles", func (t *testing.T) {
-        rect := Rectangle{12.0, 6.0}
-        got := rect.Area()
-        want := 72.0
+type Shape interface {
+    Area() float64
+}
 
+func TestArea(t *testing.T) {
+
+    checkArea := func(t testing.TB, shape Shape, want float64) {
+        t.Helper()
+        got := shape.Area()
         if got != want {
             t.Errorf("got %g want %g", got, want)
         }
+    }
+
+    t.Run("rectangles", func (t *testing.T) {
+        rect := Rectangle{12.0, 6.0}
+        want := 72.0
+        checkArea(t, rect, want)
     })
 
     t.Run("circle", func (t *testing.T) {
         circle := Circle{10}
-        got := circle.Area()
         want := 314.1592653589793 
-
-        if got != want {
-            t.Errorf("got %g want %g", got, want)
-        }
+        checkArea(t, circle, want)
     })
 
 }
