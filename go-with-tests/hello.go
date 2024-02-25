3b2c3d016035e90093e108007be68b9455a9c78b
Preston Neal <mrprestonneal@gmail.com>
2024-02-24 18:45:30 -0800

    refactor

--- go-with-tests/hello.go
+++ go-with-tests/hello.go
@@ -2,15 +2,17 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishPrefix = "hello, "
+const spanishPrefix = "hola, "
 
 func Hello(name, language string) string {
     if name == "" {
         name = "world"
     }
 
-    if language == "Spanish" {
-        return "hola, " + name
+    if language == spanish {
+        return spanishPrefix + name
     }
 
     return englishPrefix + name
