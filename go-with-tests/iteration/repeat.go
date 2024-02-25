57b2cb76f410a3a45ea6cc5ee7d535c6495de077
Preston Neal <mrprestonneal@gmail.com>
2024-02-24 20:07:36 -0800

    refactor

--- go-with-tests/iteration/repeat.go
+++ go-with-tests/iteration/repeat.go
@@ -3,7 +3,7 @@ package iteration
 func Repeat(char string) string {
     var repeated string
     for range 5 {
-        repeated = repeated + char
+        repeated += char
     }
     return repeated 
 }
