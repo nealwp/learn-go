6cdc60ad4b86af676d8e976daeb5d70be020eb94
Preston Neal <mrprestonneal@gmail.com>
2024-02-24 18:32:47 -0800

    hello to name

--- go-with-tests/hello_test.go
+++ go-with-tests/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-    got := Hello()
-    want := "hello"
+    got := Hello("nealwp")
+    want := "hello, nealwp"
 
     if got != want {
         t.Errorf("got %q want %q", got, want)
