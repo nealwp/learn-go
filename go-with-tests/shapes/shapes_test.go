753def5837a64c7f788eec28b2d7d66e677c8d32
Preston Neal <mrprestonneal@gmail.com>
2024-02-25 10:11:11 -0800

    table driven tests

--- go-with-tests/shapes/shapes_test.go
+++ go-with-tests/shapes/shapes_test.go
@@ -18,24 +18,20 @@ type Shape interface {
 
 func TestArea(t *testing.T) {
 
-    checkArea := func(t testing.TB, shape Shape, want float64) {
-        t.Helper()
-        got := shape.Area()
-        if got != want {
-            t.Errorf("got %g want %g", got, want)
+    areaTests := []struct {
+        shape Shape
+        want float64
+    }{
+        {Rectangle{12, 6}, 72.0},
+        {Circle{10}, 314.1592653589793},
+    }
+
+    for _, tt := range areaTests {
+        got := tt.shape.Area()
+        if got != tt.want {
+            t.Errorf("got %g want %g", got, tt.want)
         }
     }
 
-    t.Run("rectangles", func (t *testing.T) {
-        rect := Rectangle{12.0, 6.0}
-        want := 72.0
-        checkArea(t, rect, want)
-    })
-
-    t.Run("circle", func (t *testing.T) {
-        circle := Circle{10}
-        want := 314.1592653589793 
-        checkArea(t, circle, want)
-    })
 
 }
