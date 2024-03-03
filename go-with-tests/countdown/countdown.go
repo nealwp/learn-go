65e242e4969c7fe568999f8ef8444422c5a7ca87
Preston Neal <mrprestonneal@gmail.com>
2024-03-03 10:28:05 -0800

    configurable sleeper

--- go-with-tests/countdown/countdown.go
+++ go-with-tests/countdown/countdown.go
@@ -13,11 +13,21 @@ type Sleeper interface {
     Sleep()
 }
 
+type ConfigurableSleeper struct {
+    duration time.Duration
+    sleep func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+    c.sleep(c.duration)
+}
+
 type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() {
     time.Sleep(1 * time.Second)
 }
+
 
 func Countdown(out io.Writer, sleeper Sleeper) {
     for i := countdownStart; i > 0; i-- {
--- go-with-tests/countdown/countdown_test.go
+++ go-with-tests/countdown/countdown_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"reflect"
 	"testing"
+	"time"
 )
 
 type SpySleeper struct {
@@ -63,4 +64,23 @@ func TestCountdown(t *testing.T){
             t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
         }
     })
+}
+
+type SpyTime struct {
+    durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+    s.durationSlept = duration
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+    sleepTime := 5 * time.Second
+    spyTime := &SpyTime{}
+    sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+    sleeper.Sleep()
+
+    if spyTime.durationSlept != sleepTime {
+        t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+    }
 }
